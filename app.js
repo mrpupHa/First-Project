345aff1d0b7b7bf8a54a907b1f8cddfd6b9b2dae
mrbuba <wwpptew101@gmail.com>
2024-09-11 21:27:57 +0700

    static file added
    static file added

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-import express from "express";
-import debug from "debug";
-import morgan from "morgan";
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+// Create a URL object from import.meta.url
+
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"public/")));
 
 app.get("/", (req, res) => {
     res.send("Hello");
