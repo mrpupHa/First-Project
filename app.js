b1d10181cc2c45e534eb712cbaecab919533c093
mrbuba <wwpptew101@gmail.com>
2024-09-14 15:59:47 +0700

    products added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const products = require("./data/products.json")
+const productRouter = express.Router();
+
 
 // Create a URL object from import.meta.url
 
@@ -9,16 +12,31 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"public/")));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index");
+// Define routes
+productRouter.route('/').get((req, res) => {
+    res.render("products", {
+        products,
+    })
+});
 
-})
+productRouter.route('/:id').get((req, res) => {
+    const id = req.params.id;
+    res.send('Hello boy # ' + id);
+});
+
+
+app.use('/products', productRouter);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 
 app.listen(port, () => {
